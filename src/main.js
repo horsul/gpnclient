7c57318d76c0a457b5cd1ad6071382ef21d0fcfc
horsul <horsul@yandex.ru>
2022-02-28 23:08:34 +0300

    axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+import config from './config'
 
-createApp(App).mount('#app')
+const http = axios.create({
+  baseURL: config.server,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+const app = createApp(App)
+app.use(VueAxios, http)
+app.mount('#app')
